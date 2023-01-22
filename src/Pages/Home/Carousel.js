1481ec13a3f8107f57b6e31f356858b66e75e9ca
naeemtalukder <naeemtalukder169135@gmail.com>
2023-01-22 22:14:23 +0600

    accent color update

--- src/Pages/Home/Carousel.js
+++ src/Pages/Home/Carousel.js
@@ -7,25 +7,26 @@ import img4 from '../../images/img/img-4.jpg';
 const Carousel = () => {
     return (
         <div className="carousel w-full">
+        
             <div id="slide1" className="carousel-item relative w-full">
-                <img src={img1} alt='' className="w-full" /> /
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn btn-circle">❮</a>
-                    <a href="#slide2" className="btn btn-circle">❯</a>
-                </div>
-            </div>
-            <div id="slide2" className="carousel-item relative w-full">
                 <img src={img2} alt='' className="w-full" /> /
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide1" className="btn btn-circle">❮</a>
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
-            <div id="slide3" className="carousel-item relative w-full">
+            <div id="slide2" className="carousel-item relative w-full">
                 <img src={img3} alt='' className="w-full" /> /
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide2" className="btn btn-circle">❮</a>
                     <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src={img1} alt='' className="w-full" /> /
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide4" className="carousel-item relative w-full">
@@ -35,6 +36,7 @@ const Carousel = () => {
                     <a href="#slide1" className="btn btn-circle">❯</a>
                 </div>
             </div>
+            
         </div>
     );
 };
